11616c395132ee3b5b0d99abd9745d8a21332caa
Codingway-Amber <amberreine7@gmail.com>
2024-10-13 16:43:11 -0500

    test

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let imgOops, imgClutch, imgEnem, imgPlat, imgShrine, imgWhat, imgElp, imgEly, imgMheg, imgWol, imgScalar;
+
+function preload() {
+  imgOops = createImg("1botw-oops.gif");
+  imgClutch = createImg("1clutch.gif");
+  imgEnem = loadImage("botw-enemies.jpg");
+  imgPlat = loadImage("botw-plateau.jpg");
+  imgShrine = loadImage("botw-shrine.jpg");
+  imgWhat = loadImage("btotk-what.jpg");
+  imgElp = loadImage("elli-elpis.jpg");
+  imgEly = loadImage("elli-elysion.jpg");
+  imgMheg = loadImage("elli-mheg.jpg");
+  imgWol = loadImage("elli-wol.jpg");
+  // imgScalar
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 800); 
+  // imgScalar = 790/527
 }
 
 function draw() {
-  background(220);
+  background(220); 
+  // image(img, 0, 0, 100, 200);
 }
